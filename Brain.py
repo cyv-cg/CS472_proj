eb44636e5d0e4ba51cb2c373b505e62801fd56e4
Chris Grassi <cyvet.cs@gmail.com>
2022-10-12 19:27:26 -0400

    implemented neural networks so the little guys can think :)

---
+++ Brain.py
@@ -0,0 +1,95 @@
+from matplotlib.pyplot import connect
+import numpy as np
+
+class Node:
+    transformation = lambda x: x
+    
+    _activation : float = 0
+    
+    # -1 = undefined
+    # 0 = input
+    # 1 = output
+    # 2 = internal
+    neuron_type : int = -1
+    
+    def __init__(self, type : int, transformation_function) -> None:
+        self.transformation = transformation_function
+        self.neuron_type = type
+
+class Connection:
+    weight : float = 0
+    #active : bool = False
+    
+    input : int = -1
+    output : int = -1
+    
+    def __init__(self, input : int, output : int, weight : float) -> None:
+        self.weight = weight
+        self.input = input
+        self.output = output
+    
+    def set_active(self, active_state : bool) -> None:
+        self.active = active_state
+
+class Network:
+    nodes : list[Node] = None
+    connections : list[Connection] = None
+    
+    def __init__(self, nodes : list[Node], connections : list[Connection]) -> None:
+        self.nodes = nodes
+        self.connections = connections
+    
+    def reset_activations(self):
+        for n in self.nodes:
+            n._activation = 0
+    
+    # I don't know how this works but it does.
+    # It's some kind of janky reverse DFS-style algorithm.
+    def evaluate(self) -> any:
+        # Search through all connections in reverse (i.e. start at the outputs and recurse back to the inputs).
+        for node in self.nodes:
+            if node.neuron_type != 1:
+                continue
+            else:
+                node._activation = self._eval_node(node)
+        # Find the most active neuron and return its function.
+        max_activation : Node = None
+        for n in self.nodes:
+            # Only look for output neurons.
+            if n.neuron_type != 1:
+                continue
+            if max_activation == None or n._activation > max_activation._activation:
+                max_activation = n
+        self.reset_activations()
+        return max_activation.transformation
+    def _eval_node(self, node : Node) -> float:
+        # Search all connections and find every node that outputs to the given node.
+        inputs : list[Connection] = []
+        for conn in self.connections:
+            if self.nodes[conn.output] == node:
+                inputs.append(conn)
+        # Loop through each connection that leads into the given node.
+        if len(inputs) > 0:
+            activation_sum : float = 0
+            for c in inputs:
+                # No cycles.
+                # Cycles bad.
+                if c.input == c.output:
+                    continue
+                # Recursively evaluate input nodes and add together all their activations.
+                # Avoid evaluating nodes more than once.
+                # Technically, a node can have 0 activation and still have been evaluated, but it's not a huge deal.
+                if self.nodes[c.input]._activation != 0:
+                    activation_sum += self.nodes[c.input]._activation * c.weight
+                else:
+                    activation_sum += self._eval_node(self.nodes[c.input]) * c.weight
+            # If this is an output neuron, then take the transformation to get the final activation.
+            if node.neuron_type != 1:
+                node._activation = node.transformation(activation_sum)
+            # Apply the calculated activation to interior neurons.
+            else:
+                node._activation = activation_sum
+        # If no such connections exist, then this is (most likely) an input node whose activation can be taken as is.
+        elif node.neuron_type == 0:
+            node._activation = node.transformation()
+        return node._activation
--- driver.py
+++ driver.py
@@ -1,20 +1,60 @@
 from World import World
 from LittleGuy import LittleGuy
+from Brain import *
+import numpy as np
 
 world : World = World(width=8, height=8)
 population : list[LittleGuy] = []
-for _ in range(5):
-    population.append(LittleGuy(world))
-for p in population:
-    p.place_radomly()
+#for _ in range(4):
+#    population.append(LittleGuy(world))
+#for p in population:
+#    p.place_radomly()
 
-world.print_matrix()
+#world.print_matrix()
 
-for _ in range(5):
+#for _ in range(100):
+#    print()
+#    for x in population:
+#        x.move_random()
+#    world.print_matrix()
+
+##killed : int = world.kill(0, 0, world.width // 2, world.height)
+#killed : int = world.kill(1, 1, world.width - 1, world.height - 1)
+#print()
+#world.print_matrix()
+#print(f"killed {killed} little guys")
+
+
+
+
+john : LittleGuy = LittleGuy(world)
+john.brain = Network(
+    [
+        Node(type=0, transformation_function=john.check_forward), #0
+        Node(2, lambda x: 1 if x == 0 else -1), #1
+        Node(1, john.move_forward), #2
+        Node(1, john.move_random), #3
+        Node(0, john.check_time), #4
+        Node(2, lambda x: 2 / np.exp(((x - 4) / 2)**2) if x < 5 else 0), #5
+        Node(0, john.rand) #6
+    ],
+    [
+        Connection(input=0, output=1, weight=1), 
+        Connection(1, 2, 1), 
+        Connection(1, 3, -1),
+        Connection(4, 5, 1),
+        Connection(5, 3, 1.5),
+        Connection(6, 5, 0.8)
+    ]
+)
+john.place_radomly()
+
+for t in range(10):
+    world.age = t
     print()
-    population[0].move_random()
+    
+    f = john.brain.evaluate()
+    print(f)
+    f()
+    
     world.print_matrix()
-
-world.kill(world.height // 2, world.width)
-print()
-world.print_matrix()
