788b95b23ac46e185a72b3d535e0e2b0b33b3484
Chris Grassi <cyvet.cs@gmail.com>
2022-11-02 12:16:28 -0400

    added mutations and repopulation and basically finished the project but i don't know if it works yet so maybe not

--- Brain.py
+++ Brain.py
@@ -1,5 +1,6 @@
-from matplotlib.pyplot import connect
-import numpy as np
+import random as r
+from sys import intern
+import behaviors
 
 class Node:
     transformation = lambda x: x
@@ -19,7 +20,6 @@ class Node:
 
 class Connection:
     weight : float = 0
-    #active : bool = False
     
     input : int = -1
     output : int = -1
@@ -36,9 +36,15 @@ class Network:
     nodes : list[Node] = None
     connections : list[Connection] = None
     
-    def __init__(self, nodes : list[Node], connections : list[Connection]) -> None:
+    MAX_CONNECTIONS : int = 0
+    MAX_INTERNAL_NEURONS : int = 0
+    
+    def __init__(self, nodes : list[Node], connections : list[Connection], max_conn : int = 0, max_internal : int = 0) -> None:
         self.nodes = nodes
         self.connections = connections
+        
+        self.MAX_CONNECTIONS = max_conn
+        self.MAX_INTERNAL_NEURONS = max_internal
     
     def reset_activations(self):
         for n in self.nodes:
@@ -47,24 +53,31 @@ class Network:
     
     # I don't know how this works but it does.
     # It's some kind of janky reverse DFS-style algorithm.
-    def evaluate(self) -> any:
+    def evaluate(self, agent) -> any:
         # Search through all connections in reverse (i.e. start at the outputs and recurse back to the inputs).
         for node in self.nodes:
             if node.neuron_type != 1:
                 continue
             else:
-                node._activation = self._eval_node(node)
+                node._activation = self._eval_node(node, agent)
         # Find the most active neuron and return its function.
         max_activation : Node = None
+        max_activations : list[Node] = []
         for n in self.nodes:
             # Only look for output neurons.
             if n.neuron_type != 1:
                 continue
             if max_activation == None or n._activation > max_activation._activation:
                 max_activation = n
+        for n in self.nodes:
+            if n.neuron_type != 1:
+                continue
+            if max_activation == None or n._activation == max_activation._activation:
+                max_activations.append(n)
         self.reset_activations()
-        return max_activation.transformation
-    def _eval_node(self, node : Node) -> float:
+        choice = r.choice(max_activations)
+        return choice.transformation, choice._activation
+    def _eval_node(self, node : Node, agent) -> float:
         node._visited = True
         # Search all connections and find every node that outputs to the given node.
         inputs : list[Connection] = []
@@ -85,14 +98,73 @@ class Network:
                 if self.nodes[c.input]._activation != 0 or self.nodes[c.input]._visited:
                     activation_sum += self.nodes[c.input]._activation * c.weight
                 else:
-                    activation_sum += self._eval_node(self.nodes[c.input]) * c.weight
+                    activation_sum += self._eval_node(self.nodes[c.input], agent) * c.weight
             # If this is an output neuron, then take the transformation to get the final activation.
             if node.neuron_type != 1:
-                node._activation = node.transformation(activation_sum)
+                if node.neuron_type == 0:
+                    node._activation = node.transformation(agent)
+                elif node.neuron_type == 2:
+                    node._activation = node.transformation(agent, activation_sum)
             # Apply the calculated activation to interior neurons.
             else:
                 node._activation = activation_sum
         # If no such connections exist, then this is (most likely) an input node whose activation can be taken as is.
         elif node.neuron_type == 0:
-            node._activation = node.transformation()
+            node._activation = node.transformation(agent)
         return node._activation
+    
+    def mutate(self, mutation_chance : float = 1/1000) -> None:
+        internal_neurons = []
+        for n in self.nodes:
+            if n.neuron_type == 2:
+                internal_neurons.append(n)
+
+        if r.random() < mutation_chance and len(internal_neurons) > 0:
+            n = r.choice(internal_neurons)
+            i = -1
+            for x in range(len(self.nodes)):
+                if self.nodes[x] == n:
+                    i = x
+                    break
+            for c in self.connections:
+                if c.output == i or c.input == i:
+                    self.connections.remove(c)
+            self.nodes.remove(n)
+            for c in self.connections:
+                if c.input > i:
+                    c.input -= 1
+                if c.output > i:
+                    c.output -= 1
+        if r.random() < mutation_chance and len(internal_neurons) < self.MAX_INTERNAL_NEURONS:
+            self.nodes.append(
+                    Node(
+                        type=2,
+                        transformation_function=r.choice([
+                            behaviors.sigmoid,
+                            behaviors.step,
+                            behaviors.abs,
+                            behaviors.piece,
+                            behaviors.parabola
+                        ])
+                    )
+                )
+            
+        if r.random() < mutation_chance and len(self.connections) > 0:
+            self.connections.remove(r.choice(self.connections))
+        if r.random() < mutation_chance and len(self.connections) < self.MAX_CONNECTIONS:
+            self.connections.append(
+                    Connection(
+                        input=r.choice(range(len(self.nodes))), 
+                        output=r.choice(range(len(self.nodes))),
+                        weight=10 * (2 * r.random() - 1)
+                    )
+                )
+
+        for c in self.connections:
+            if r.random() < mutation_chance:
+                c.weight += r.random() - 1
+            
+            if r.random() < mutation_chance:
+                c.input = (c.input + (r.choice([-1, 1]))) % len(self.nodes)
+            if r.random() < mutation_chance:
+                c.output = (c.output + (r.choice([-1, 1]))) % len(self.nodes)
